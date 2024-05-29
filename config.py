8c7616932c6fe50f3a23aefc98545df688c98952
kqbu.mp4 <51759890+KabirKoratkar@users.noreply.github.com>
2024-05-28 21:03:45 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+OPENAI_API_KEY = 'key'
+ASSEMBLYAI_API_KEY = 'your_assemblyai_api_key'
+ELEVENLABS_API_KEY = 'your_elevenlabs_api_key'
--- dutchbot.py
+++ dutchbot.py
@@ -1,7 +1,3 @@
-OPENAI_API_KEY = 'key'
-ASSEMBLYAI_API_KEY = 'your_assemblyai_api_key'
-ELEVENLABS_API_KEY = 'your_elevenlabs_api_key'
-
 import openai
 import requests
 import time
